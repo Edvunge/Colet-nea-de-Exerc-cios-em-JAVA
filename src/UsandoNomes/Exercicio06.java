59f9dc3a3038e3977b0e5e4012ba0aea570b1276
compu <edvaldolaurentina07@hotmail.com>
2022-02-13 01:55:58 +0000

    Exercicio06

---
+++ src/UsandoNomes/Exercicio06.java
@@ -0,0 +1,35 @@
+package UsandoNomes;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Exercicio06 {
+    public static void main(String[] args) throws IOException {
+
+        /*
+        6) Ler nome, sexo e idade. Se sexo for feminino e idade menor que
+        25. Imprimir o nome da pessoa e a palavra ACEITA. Caso contrario
+        imprimir NAO ACEITA.
+        */
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(" indique o seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println(" indique o seu sexo: ");
+        char sexo = (char)System.in.read(); //sc.nextLine();
+
+        System.out.println(" indique a sua idade: ");
+        int idade = sc.nextInt();
+
+        if ( (sexo == 'f') || ( sexo == 'F' ) && idade < 25){
+            System.out.printf("%s. ACEITA." , nome);
+        } else {
+            System.out.println("NAO ACEITA.");
+        }
+
+        sc.close();
+
+    }
+}
