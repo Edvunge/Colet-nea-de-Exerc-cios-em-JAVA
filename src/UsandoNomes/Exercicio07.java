92d391651156ad8af2869e60b738cf43fccc6a83
compu <edvaldolaurentina07@hotmail.com>
2022-02-13 03:04:04 +0000

    retificando o Exercicio07

--- src/UsandoNomes/Exercicio07.java
+++ src/UsandoNomes/Exercicio07.java
@@ -10,24 +10,10 @@ public class Exercicio07 {
 
         Scanner sc = new Scanner(System.in);
 
-        /*
         System.out.println(" indique o seu nome: ");
         String nome = sc.nextLine();
 
         System.out.println(" contem  " + nome.length() + " letras ");
-        */
-
-        int x, tam;
-
-
-        System.out.println(" indique o seu nome: ");
-        String nome = sc.nextLine();
-
-        tam = nome.length();
-
-        for (x = 1; x <= tam; x++){
-            System.out.printf(" %s ", nome);
-         }
 
         sc.close();
     }
